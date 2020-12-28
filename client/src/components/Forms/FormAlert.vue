4208cb020b633ccef0fa1b44f20f1699f9bdf6e9
Ismael Ordás <ismael.ordas@mimacom.com>
2020-12-28 12:46:09 +0100

    FormAlert component

---
+++ client/src/components/Forms/FormAlert.vue
@@ -0,0 +1,39 @@
+<template>
+<div class="form-alert" :class='cssClass'>
+  <p>error</p>
+</div>
+</template>
+
+<script>
+export default {
+  name: "FormAlert",
+  props: {
+    type: {
+      type: String,
+      required: false
+    },
+    show: {
+      type: Boolean,
+      required: true
+    }
+  },
+  computed: {
+    cssClass () {
+      return {
+        'hidden': !this.show,
+        'rounded': true,
+        'text-sm': true,
+        'mb-4': true,
+        'p-2': true,
+        'bg-red-400': this.type === 'error',
+        'bg-green-400': this.type !== 'error'
+      }
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
