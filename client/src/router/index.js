8b2c61c7fc6fc531261eb74ac2a9c89bee439546
Ismael Ordás <ismael.ordas@mimacom.com>
2021-01-03 10:20:18 +0100

    Add routeGuard for loggedIn users

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
+import store from '@/store';
 import Home from "../views/Home.vue";
 import SignIn from "../views/SignIn";
 import SignUp from "../views/Signup";
@@ -7,21 +8,33 @@ const routes = [
   {
     path: "/",
     name: "Home",
-    component: Home
+    component: Home,
+    meta: {
+      requiresAuth: false
+    }
   },
   {
     path: "/signin",
     name: "SignIn",
-    component: SignIn
+    component: SignIn,
+    meta: {
+      requiresAuth: false
+    }
   },
   {
     path: "/signup",
     name: "SignUp",
-    component: SignUp
+    component: SignUp,
+    meta: {
+      requiresAuth: false
+    }
   },
   {
     path: "/about",
     name: "About",
+    meta: {
+      requiresAuth: false
+    },
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
@@ -34,5 +47,17 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 });
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (!store.getters["user/isLoggedIn"]) {
+      next({ name: 'SignIn' })
+    } else {
+      next()
+    }
+  } else {
+    next()
+  }
+})
 
 export default router;
